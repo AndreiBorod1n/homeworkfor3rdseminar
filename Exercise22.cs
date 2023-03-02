8a07c0ab252e05403a7ae3e2b183c9e496e0417e
Andrei <andrei.borod1n@mail.ru>
2023-03-02 23:07:43 +0500

    Add exercises 21 and 22

---
+++ Exercise22.cs
@@ -0,0 +1,12 @@
+﻿// 22. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine()); // Array Length
+int[] arr = new int[N];
+int power = 3;
+for (int index = 0; index < N; index++)
+{
+    arr[index] = index + 1;
+    arr[index] = Convert.ToInt32(Math.Pow(arr[index], power));
+    System.Console.Write(arr[index]);
+    Console.Write(' ');
+}
